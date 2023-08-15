2202a3b625bb79f8bbbd1a4637ede5dccedf1209
lvg1610 <64771079+lvg1610@users.noreply.github.com>
2023-08-15 22:09:04 +0530

    Create Circle.java
    Circle.java added first

---
+++ Circle.java
@@ -0,0 +1,13 @@
+public class Circle
+{
+    private double radius;
+    public Circle(){}
+    public void setRadius(double radius)
+    {
+        this.radius = radius;
+    }
+    public double getRadius()
+    {
+        return radius;
+    }  
+}    
