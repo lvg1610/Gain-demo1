b283f1e4520dd8870732a7412e0e29355f49ac0c
lvg1610 <64771079+lvg1610@users.noreply.github.com>
2023-08-16 06:15:52 +0530

    parameterized cons added to Circle.java 1.1V

--- Circle.java
+++ Circle.java
@@ -2,6 +2,10 @@ public class Circle
 {
     private double radius;
     public Circle(){}
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
     public void setRadius(double radius)
     {
         this.radius = radius;
